416a6ca8ee4fffbac2a0afc1d38e300f419fb8ca
MaiteVillalon1 <>
2022-05-13 00:28:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -93,8 +93,8 @@ int is_valid(Node* n){
     {
       int i=3*(k/3) + (p/3) ;
       int j=3*(k%3) + (p%3) ;
-      printf("%d ",n->sudo[i][j]);
-      if(p%3 == 2) printf("\n");
+      //printf("%d ",n->sudo[i][j]);
+      //if(p%3 == 2) printf("\n");
 
       int d = n->sudo[j][i];
       if (d != 0)
